b456d856717e251db9c9ebb8631bf1c414d28971
yinping <yp_ren@hotmail.com>
2016-09-17 23:00:09 +0800

    ss

---
+++ packages/update_ignore_list.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -e -o pipefail
+
+wget -O- 'http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest' | \
+    awk -F\| '/CN\|ipv4/ { printf("%s/%d\n", $4, 32-log($5)/log(2)) }' > \
+    /tmp/ignore.list
+
+mv /tmp/ignore.list /etc/
+
