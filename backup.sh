bbd4e540746319243eab6ba0815c767c601f5f24
yinping <yp_ren@hotmail.com>
2016-09-18 01:04:25 +0800

    ...

--- backup.sh
+++ backup.sh
@@ -1,17 +1,23 @@
 # /bin/sh
 
-DL_DIR="www/dl";
-BAK_FILE="${DL_DIR}/backup-`cat /proc/sys/kernel/hostname`";
-BAK_FILE="${BAK_FILE}-15.05.1-ar71xx-nand-wndr3700v4";
+DL_DIR="/www/dl";
+rm -rf ${DL_DIR};
+
+BAK_FILE="/tmp/backup";
 [ ! -z "$1" ] && BAK_FILE="${BAK_FILE}-$1";
+BAK_FILE="${BAK_FILE}-`cat /proc/sys/kernel/hostname`";
+BAK_FILE="${BAK_FILE}-15.05.1-ar71xx-nand-wndr3700v4";
 BAK_FILE="${BAK_FILE}-`date +%F`";
 BAK_FILE="${BAK_FILE}.tar.gz";
-EXCLUDE="--exclude=${DL_DIR} --exclude=root";
+
+EXCLUDE="--exclude=root";
 BAK_LIST="bin etc lib mnt sbin usr www";
 
 cd /overlay/upper;
-mkdir -p /${DL_DIR};
 tar -zcf "${BAK_FILE}" ${EXCLUDE} ${BAK_LIST};
 cd /root;
 
-./aes-256-cfb.sh /${BAK_FILE}
+./aes-256-cfb.sh ${BAK_FILE};
+mkdir -p ${DL_DIR};
+mv ${BAK_FILE}.aes-256-cfb ${DL_DIR}/;
+rm -rf ${BAK_FILE};
